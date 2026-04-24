e9eecaad439f0792049974379d29a11b6e6e872c
Oscar <56079903+OscarNOW@users.noreply.github.com>
2026-04-24 20:53:32 +0200

    improved idea

--- idea.ts
+++ idea.ts
@@ -2,9 +2,15 @@ const handler = lrHandler('*', '/', async req => {
     return lrNext();
     return lrJson({ success: true });
     return lrStatus(500, lrJson({ success: false }));
+    return lrRedirect('/');
 });
 
-const router = lrRouter();
+// a router is a type of handler
+const router = lrRouter([
+    handler,
+    router2,
+    handler2
+]);
 
 const app = lrApp(router);
 
