3470136d346bfe0e15bcf7a22900a20be17f2806
Oscar <56079903+OscarNOW@users.noreply.github.com>
2026-04-29 18:21:57 +0200

    Created defaultStatusMessages

--- index.ts
+++ index.ts
@@ -24,6 +24,9 @@ const defaultStatusMessages = {
     410: 'Gone',
     413: 'Content Too Large',
     415: 'Unsupported Media Type',
+    500: 'Internal Server Error',
+    501: 'Not Implemented',
+    503: 'Service Unavailable',
 };
 
 type responseBody = {
